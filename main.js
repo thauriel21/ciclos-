4e181d5cb95b6254ba03455290bf72e514cd880c
Alynson Roldan <alynsonrolfu@gmai.com>
2022-09-27 13:21:43 -0300

    cree un archivo index y un archivo main

---
+++ main.js
@@ -0,0 +1,19 @@
+// let n=1
+//do{
+ //   console.log(n);
+  //  n=n+1
+//}while(n<=10)
+
+//let n2=10
+//do{
+  //  console.log(n2);
+    //n2=n2-1
+//}while(n2>=1)
+
+
+const name= "alynson"
+let n =name.length-1
+do{
+    console.log(name[n])
+    n--
+}while(n>=0)
